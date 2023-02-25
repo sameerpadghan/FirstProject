156fffb0cfc527c547d12a323aea9ee9223f40fc
sameerpadghan <96347400+sameerpadghan@users.noreply.github.com>
2023-02-25 09:22:18 +0530

    Added a UObject  child class UMyObject
    1)Made the class Blueprintable
    2)Made the my_variable BlueprintReadWrite
    3)Made the my_function BlueprintCallable

---
+++ Source/FirstProject/MyObject.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "MyObject.generated.h"
+
+/**
+ *
+ */
+ //Keywords used when declaring UClasses to specify how the class behaves with various aspects of the Engine and Editor.
+ //Link:https://docs.unrealengine.com/4.27/en-US/ProgrammingAndScripting/GameplayArchitecture/Classes/Specifiers/
+ //Making the class blueprintable
+UCLASS(Blueprintable)
+class FIRSTPROJECT_API UMyObject : public UObject
+{
+	GENERATED_BODY()
+public:
+	//Making the varible visible in the Blueprint
+	UPROPERTY(BlueprintReadWrite)
+		float my_float = 0;
+	//Making the function visibile in Blueprint
+	UFUNCTION(BlueprintCallable)
+		float my_function(float value);
+};
